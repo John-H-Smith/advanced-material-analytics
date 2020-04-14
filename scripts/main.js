65120f4d9028c9a924cae1b29b2d1be2d7dae587
John_H_Smith <derFabi95@gmail.com>
2020-04-14 14:00:24 +0200

    Added Tabs

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,16 @@ var ready = ( callback ) => {
 } 
 g.e = function( selektor ){ return document.querySelector( selektor ); };
 g.eAll  = function( selektor ){ return document.querySelectorAll( selektor ); };
+
+ready( menu );
+
+function menu() {
+
+    let size = 0;
+    g.eAll( 'main > div' ).forEach(ele => {
+        if(ele.offsetWidth > size)
+            if(ele.style.display != "none")
+                size = ele.offsetWidth;
+    });
+    g.e( 'header ul' ).style.maxWidth = size + "px";
+}
