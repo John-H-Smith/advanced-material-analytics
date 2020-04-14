0720f17a68041c0144b7e3852f6b9e75954aee12
John_H_Smith <derFabi95@gmail.com>
2020-04-14 12:06:52 +0200

    removed duplicate ready function

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,3 @@ var ready = ( callback ) => {
 } 
 g.e = function( selektor ){ return document.querySelector( selektor ); };
 g.eAll  = function( selektor ){ return document.querySelectorAll( selektor ); };
-
-// document.ready without jquery
-let ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
