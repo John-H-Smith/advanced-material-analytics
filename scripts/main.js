61c980d48e99480351da6ddf39f7febcdf767312
John_H_Smith <derFabi95@gmail.com>
2020-04-14 11:45:34 +0200

    added all selector

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ g.get = function(url) { //answer is json => d.json() returns the answer
     return fetch(url).then(d => d.json(), e => g.log(e)).catch(e => g.log(e));
 }
 g.e = function(element) { return document.querySelector(element); };
+g.eAll  = function( selektor ){ return document.querySelectorAll( selektor ); };
 
 // document.ready without jquery
 let ready = (callback) => {
