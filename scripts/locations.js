38c9939dcb8b0443bf46d84b072134f04c69bd3b
Thomas <thomas.binder@edvschule-plattling.de>
2020-05-23 12:30:25 +0200

    Better upper-/lowercase writing, statistics with € label, faster active/inactive statistic, better locations

--- scripts/locations.js
+++ scripts/locations.js
@@ -1,5 +1,10 @@
 g.url = "https://ux5.edvschulen-plattling.de/sap/opu/odata/sap/ZMITO_LOCATION_SRV/location_entity_set";
 g.format = "/?$format=json"
+var adress;
+g.url2 = "https://nominatim.openstreetmap.org/search?q="
+g.format2 = "&format=json";
+var maps = [];
+
 
 ready(main);
 
@@ -12,14 +17,24 @@ function read(url) {
     g.count += parseInt(1);
     g.get(url + g.format).then(d => {
         console.log(d);
-        showData(d);
+        createMap(d);
     });
 }
 
-function showData(d) {
-    d.d.results.forEach((e, i) => {
-        if (i == 0) {
-            g.e("#_mapTitle").innerHTML += e.NAME + ": " + e.CITY + " " + e.STREET + " " + e.HOUSNUMBER + " " + e.ZIP + "<br>";
-        }
+
+function createMap(d) {
+    d.d.results.forEach((ld, i) => {
+        g.get(g.url2 + ld.STREET + "-" + ld.HOUSNUMBER + ",+" + ld.CITY + g.format2).then(e => {
+            g.e("#main").innerHTML += "<div id=\"div" + i + "\" ><iframe id=\"map" + i + "\" width=\"425\" height=\"350\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" style=\"border: 1px solid black\"></iframe><br></div>";
+            maps.push(g.e("#map" + i));
+            maps[i].setAttribute("src", "https://www.openstreetmap.org/export/embed.html?bbox=" + e[0].boundingbox[2] + "%2C+" + e[0].boundingbox[1] + "%2C+" + e[0].boundingbox[2] + "%2C" + e[0].boundingbox[0] + "&amp;layer=mapnik&amp");
+            let title = document.createElement("div")
+            title.setAttribute("class", "mapTitle");
+            title.setAttribute("id", "mapTitle" + i);
+            title.innerHTML = ld.NAME + ": " + ld.CITY + " " + ld.STREET + " " + ld.HOUSNUMBER + "<br>";
+            g.e("#div" + i).appendChild(title);
+
+
+        });
     });
 }
