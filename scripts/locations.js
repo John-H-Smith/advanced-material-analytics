e9be4219412e9e8966810d9abf1ba040c4a12d10
Fabian Heinz <derFabi95@gmail.com>
2020-05-27 11:52:24 +0200

    Update for location design

--- scripts/locations.js
+++ scripts/locations.js
@@ -25,16 +25,9 @@ function read(url) {
 function createMap(d) {
     d.d.results.forEach((ld, i) => {
         g.get(g.url2 + ld.STREET + "-" + ld.HOUSNUMBER + ",+" + ld.CITY + g.format2).then(e => {
-            g.e("#main").innerHTML += "<div id=\"div" + i + "\" ><iframe id=\"map" + i + "\" width=\"425\" height=\"350\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" style=\"border: 1px solid black\"></iframe><br></div>";
+            g.e("#main").innerHTML += "<iframe id=\"map" + i + "\" width=\"425\" height=\"350\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" style=\"border: 1px solid black\"></iframe><p class='mapTitle'>" + ld.NAME + "<br />" + ld.STREET + " " + ld.HOUSNUMBER +  "<br />" + ld.CITY + "</p>";
             maps.push(g.e("#map" + i));
             maps[i].setAttribute("src", "https://www.openstreetmap.org/export/embed.html?bbox=" + e[0].boundingbox[2] + "%2C+" + e[0].boundingbox[1] + "%2C+" + e[0].boundingbox[2] + "%2C" + e[0].boundingbox[0] + "&amp;layer=mapnik&amp");
-            let title = document.createElement("div")
-            title.setAttribute("class", "mapTitle");
-            title.setAttribute("id", "mapTitle" + i);
-            title.innerHTML = ld.NAME + ": " + ld.CITY + " " + ld.STREET + " " + ld.HOUSNUMBER + "<br>";
-            g.e("#div" + i).appendChild(title);
-
-
         });
     });
 }
